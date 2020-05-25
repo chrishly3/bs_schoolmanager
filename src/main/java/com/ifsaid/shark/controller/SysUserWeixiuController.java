286dd1ce9f1ee30ca5f23c7c7f3f0ab1196d4226
chrishly3 <891102438@qq.com>
2020-05-25 15:40:26 +0800

    添加维修单页面展示

--- src/main/java/com/ifsaid/shark/controller/SysUserWeixiuController.java
+++ src/main/java/com/ifsaid/shark/controller/SysUserWeixiuController.java
@@ -2,18 +2,15 @@ package com.ifsaid.shark.controller;
 
 
 import com.github.pagehelper.PageInfo;
-import com.ifsaid.shark.common.controller.BaseController;
 
 import com.ifsaid.shark.entity.QueryOrder;
 import com.ifsaid.shark.entity.TbSysUserWeixiu;
 import com.ifsaid.shark.service.TbSysUserWeixiuService;
 import com.ifsaid.shark.util.JsonResult;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.ifsaid.shark.util.QueryParameter;
 
 @Slf4j
 @Api(tags = "[ 服务权限 ] 服务管理")
@@ -25,14 +22,12 @@ public class SysUserWeixiuController {
     private TbSysUserWeixiuService tbSysUserWeixiuService;
     //private final Logger logger = LoggerFactory.getLogger(SchoolUserInfoController.class);
 
-
 //    @ApiOperation(value = "分页,获取服务报修详情", notes = "分页 查询所有，获取报修详情")
 //    @GetMapping("/info/page")
 //    public JsonResult findAllUserWeixiuPage(QueryParameter parameter) {
 //        PageInfo<TbSysUserWeixiuVo> page = baseService.findAllUserWeixiuPage(parameter);
 //        return JsonResult.success(page.getTotal(), page.getList());
 //    }
-
 
 //    /**
 //     * 根据id查询维修单
@@ -61,42 +56,41 @@ public class SysUserWeixiuController {
 //        }
 //    }
 //
-//    /**
-//     * 删除报修单
-//     * @param id
-//     * @return
-//     */
-//    @DeleteMapping("{id}")
-//    public R removeById(@PathVariable String id){
-//        Integer delete= orderService.removeById(id);
-//        System.out.println(delete);
-//        if(delete.equals(1)){
-//            return R.ok();
-//        }else {
-//            return R.error();
-//        }
-//    }
+
+
 
     /**
      * 带条件分页查询
+     *
      * @param queryOrder
      * @param page
      * @param pageSize
      * @return
      */
     @PostMapping("moreCondtionPageList/{page}/{pageSize}")
-    public  JsonResult showOrders(@PathVariable Integer page, @PathVariable Integer pageSize,
-                         @RequestBody(required = false) QueryOrder queryOrder){
+    public JsonResult showOrders(@PathVariable Integer page, @PathVariable Integer pageSize,
+                                 @RequestBody(required = false) QueryOrder queryOrder) {
         PageInfo<TbSysUserWeixiu> pageList = tbSysUserWeixiuService.showOrders(page, pageSize);
+        System.out.println(pageList);
         String repairUserName = queryOrder.getRepairUserName();
         System.out.println(repairUserName);
         String repairType = queryOrder.getRepairType();
-        if(repairUserName != null || repairType != null) {
-            pageList = tbSysUserWeixiuService.selectOrderLike("%"+repairUserName+"%",page, pageSize,"%"+repairType+"%");
+        if (repairUserName != null || repairType != null) {
+            pageList = tbSysUserWeixiuService.selectOrderLike("%" + repairUserName + "%", page, pageSize, "%" + repairType + "%");
         }
-        System.out.println(pageList);
+        //System.out.println(pageList);
         return JsonResult.success(pageList);
     }
+
+    @DeleteMapping("{id}")
+    public JsonResult removeById(@PathVariable String id) {
+        Integer delete = tbSysUserWeixiuService.removeById(id);
+        System.out.println(delete);
+        if (delete.equals(1)) {
+            return JsonResult.success();
+        } else {
+            return JsonResult.error();
+        }
 
 //    /**
 //     * 查询所有报修单（测试）
@@ -127,17 +121,7 @@ public class SysUserWeixiuController {
 //        }
 //    }
 
-    @RequestMapping(value = "/addOrder",method = RequestMethod.POST)
-    public JsonResult addSchoolRepair(@RequestBody TbSysUserWeixiu schoolRepair) {
-        System.err.println(schoolRepair.getRepairUserName());
-        //System.err.println(schoolRepair.getRepairType());
-        int save = tbSysUserWeixiuService.saveOrder(schoolRepair);
-        if(save==1) {
-            return JsonResult.success();
-        } else {
-            return JsonResult.fail("添加维修单失败");
-        }
+
+
     }
-
-
 }
--- src/main/java/com/ifsaid/shark/mapper/TbSysUserWeixiuMapper.java
+++ src/main/java/com/ifsaid/shark/mapper/TbSysUserWeixiuMapper.java
@@ -1,11 +1,8 @@
 package com.ifsaid.shark.mapper;
 
-import com.ifsaid.shark.common.mapper.BaseMapper;
-import com.ifsaid.shark.entity.SysUser;
 import com.ifsaid.shark.entity.TbSysUserWeixiu;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -13,12 +10,7 @@ import java.util.List;
 @Repository
 public interface TbSysUserWeixiuMapper {
 
-    /**
-     * 查询所有保修单
-     * @return
-     */
-    @Select("select * from tb_sys_user_weixiu where def =0")
-    public List<TbSysUserWeixiu> listOrder();
+
 
     /**
      * 模糊查询报修单
@@ -29,6 +21,21 @@ public interface TbSysUserWeixiuMapper {
     @Select("select * from tb_sys_user_weixiu "
             + "where repair_userName like #{repairUserName} and repair_type like #{repairType} and def = 0")
     List<TbSysUserWeixiu> selectOrderLike(@Param("repairUserName") String repairUserName, @Param("repairType") String repairType);
+
+    /**
+     * 查询所有保修单
+     * @return
+     */
+    @Select("select * from tb_sys_user_weixiu where def =0")
+    public List<TbSysUserWeixiu> listOrder();
+
+    /**
+     * 逻辑删除报修单
+     * @param id
+     * @return
+     */
+    @Update("UPDATE tb_sys_user_weixiu SET def = 1 WHERE id=#{id}")
+    Integer removeById(String id);
 
 
 }
