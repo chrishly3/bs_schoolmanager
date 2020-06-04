bd7a46cdf8ab64d89dca3ba2e810d40f4320c9cc
chrishly3 <891102438@qq.com>
2020-06-05 01:36:33 +0800

    修改部分方法

--- src/main/java/com/ifsaid/shark/mapper/TbSysUserWeixiuMapper.java
+++ src/main/java/com/ifsaid/shark/mapper/TbSysUserWeixiuMapper.java
@@ -1,5 +1,6 @@
 package com.ifsaid.shark.mapper;
 
+import com.ifsaid.shark.entity.TbComplete;
 import com.ifsaid.shark.entity.TbSysUserEvaluate;
 import com.ifsaid.shark.entity.TbSysUserWeixiu;
 import org.springframework.stereotype.Repository;
@@ -38,12 +39,25 @@ public interface TbSysUserWeixiuMapper {
     @Update("UPDATE tb_sys_user_weixiu SET def = 1 WHERE id=#{id}")
     Integer removeById(String id);
 
-    @Select("select * from tb_sys_user_weixiu "
+    /*@Select("select * from tb_sys_user_weixiu "
             + "where repair_userName = #{loginUserName}and def = 0")
-    List<TbSysUserWeixiu> getOrders(@Param("loginUserName") String loginUserName);
+    List<TbSysUserWeixiu> getOrders(@Param("loginUserName") String loginUserName);*/
 
-    @Insert("insert into tb_sys_user_evaluate(createuser_id,repairuser_id,msgtext,creattime,def,repair_id,star) values(#{createuserId},#{repairuserId},#{msgText},#{creattime},#{def},#{repairId},#{star})")
+    @Select("select * from tb_sys_user_weixiu a   left join tb_sys_user_evaluate b on a.id= b.repair_id "
+            + "where a.repair_userName = #{loginUserName} and a.def = 0")
+    List<TbComplete> getOrders(@Param("loginUserName") String loginUserName);
+
+    @Insert("insert into tb_sys_user_evaluate(createuser_id,repairuser_id,msgtext,creattime,def,repair_id,star,status) values(#{createuserId},#{repairuserId},#{msgText},#{creattime},#{def},#{repairId},#{star},#{status})")
     Integer addEvalatedata(TbSysUserEvaluate tbSysUserEvaluate);
 
+    @Select("select * from tb_sys_user_weixiu a   left join tb_sys_user_evaluate b on a.id= b.repair_id "
+            + "where a.id = #{id} and a.def = 0")
+    List<TbComplete> getEvaluate(@Param("id") String id);
 
+    /**
+     * 修改维修单状态
+     */
+
+    @Update("UPDATE tb_sys_user_weixiu SET repair_status = #{status} WHERE id=#{id}")
+    Integer updateOrderStatus(String id,String status);
 }
